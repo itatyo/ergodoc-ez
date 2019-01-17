79c7a9a8734e66493fb982b1ebac202e92dff609
Itatyo <h.inotty@gmail.com>
2019-01-17 17:49:17 +0900

    add =

--- keymap.c
+++ keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | CAD  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| L1   | F13  |                                       |  F14 | L1   |   [  |   ]  | ~L1  |
+ *   |Grv/L1|  '"  |AltShf| L1   | F13  |                                       |  F14 | L1   |   [  |   ]  |   =  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MO(SYMB),           KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
   KC_H,         KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
   LALT(LCTL(KC_DEL)), KC_N,    KC_M,    KC_COMM, KC_DOT,            CTL_T(KC_SLSH), KC_RSFT,
-  KC_F14,             MO(SYMB),KC_LBRC, KC_RBRC, KC_FN1,
+  KC_F14,             MO(SYMB),KC_LBRC, KC_RBRC, KC_EQL,
   KC_LALT,            CTL_T(KC_ESC),
   KC_PGUP,
   KC_PGDN,            KC_DELT, KC_ENT
